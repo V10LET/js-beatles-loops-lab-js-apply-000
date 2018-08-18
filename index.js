0c0746815219831b075f75476fa94388106b0649
Violet Moon <violet.wmoon@gmail.com>
2018-08-18 08:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  for (var i = 4; i < array.length; i++) {
+    var musicians = [
+      'John Lennon',
+      'Ringo Starr',
+      'Paul McCartney',
+      'George Harrison'
+    ]
+
+    var instruments = [
+      'guitar',
+      'drums',
+      'vocals',
+      'guitar'
+    ]
+
+  }
+}
